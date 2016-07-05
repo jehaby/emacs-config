d78d8559ad85f0172d375f191836770e78a2afd4
Stas Makarov <jehaby@ya.ru>
2016-07-06 02:22:16 +0300

    Added magit global key. From magit.

--- keys.el
+++ keys.el
@@ -6,3 +6,6 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-x\C-b" 'buffer-menu)
 (global-set-key "\C-x\M-f" 'find-name-dired)
+
+;;; magit
+(global-set-key (kbd "C-x g") 'magit-status)
