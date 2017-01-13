c284b10ddf5a47eecb631c5ebc45652c2fca267a
Stas Makarov <jehaby@ya.ru>
2017-01-13 11:39:52 +0300

    gofmt before save

--- go.el
+++ go.el
@@ -4,5 +4,7 @@
 
 (add-to-list 'exec-path (concat homepath "coding/go/bin/"))
 
+(add-hook 'before-save-hook #'gofmt-before-save)
+
 (eval-after-load 'go-mode 
   '(define-key go-mode-map (kbd "C-M-g") 'godef-jump))
